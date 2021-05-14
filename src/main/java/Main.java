777007118bdb9d3214793e4f6bab6f7db4d8a914
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-15 01:09:41 +0800

    Add new Launcher for JavaFx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,39 +2,19 @@ package main.java;
 
 import java.util.Scanner;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-    public static String command = "";
+public class Main extends Application {
 
-    /**
-     * Scanner to read input from the console
-     **/
-    public static final Scanner SCANNER = new Scanner(System.in);
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
 
-
-    public static void main(String[] args) {
-	// write your code here
-
-        System.out.println("Hello! What is your name?");
-        String username = SCANNER.nextLine();
-        System.out.println("Hello there " + username + "! Give a command!");
-
-
-        while (true){
-            command = SCANNER.nextLine();
-
-            if (command.equals("greet")){
-                System.out.println("Hello again username!");
-            }
-            else if (command.equals("joke")){
-                System.out.println("I have no jokes lmao");
-            } else if (command.equals("exit") || command.equals("bye")) {
-                System.out.println("Bye!");
-                break;
-            } else {
-                System.out.println("I don't understand the command?");
-            }
-        }
-
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
     }
 }
