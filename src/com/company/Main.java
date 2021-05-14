cd13db4c6e5db9ce08ef0851bf172d1fa0c6d7c8
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-14 17:44:03 +0800

    Add basic interaction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static String command = "";
+
     /**
      * Scanner to read input from the console
      **/
@@ -15,7 +17,24 @@ public class Main {
 
         System.out.println("Hello! What is your name?");
         String username = SCANNER.nextLine();
-        System.out.println("Hello there " + username + "!");
+        System.out.println("Hello there " + username + "! Give a command!");
+
+
+        while (true){
+            command = SCANNER.nextLine();
+
+            if (command.equals("greet")){
+                System.out.println("Hello again username!");
+            }
+            else if (command.equals("joke")){
+                System.out.println("I have no jokes lmao");
+            } else if (command.equals("exit") || command.equals("bye")) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("I don't understand the command?");
+            }
+        }
 
     }
 }
