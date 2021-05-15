af1c4f4f6a37e693034df122e428afcdc90f36cd
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-15 23:17:19 +0800

    Testing different placements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -24,26 +25,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        primaryStage.setTitle("My First JavaFX GUI");
-
-        //Scene 1
-        Label label1 = new Label("This is the first scene");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> primaryStage.setScene(scene2));
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 300, 250);
-
-        //Scene 2
-        Label label2 = new Label("This is the second scene");
-        Button button2 = new Button("Go to scene 1");
-        button2.setOnAction(e -> primaryStage.setScene(scene1));
-        VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(label2, button2);
-        scene2 = new Scene(layout2, 300, 250);
-
-
-        primaryStage.setScene(scene1);
+        BorderPane BPane = new BorderPane();
+        BPane.setTop(new Label("This will be at the top"));
+        BPane.setLeft(new Label("This will be at the left"));
+        BPane.setRight(new Label("This will be at the Right"));
+        BPane.setCenter(new Label("This will be at the Centre"));
+        BPane.setBottom(new Label("This will be at the bottom"));
+        Scene scene = new Scene(BPane,600,400);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
