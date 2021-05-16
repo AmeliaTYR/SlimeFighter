1e4393c24ef8b66f8165b08214fd5d6532d025c4
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-16 21:56:56 +0800

    Broken fxml archive

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,43 @@
 package main.java;
 
-import java.util.Scanner;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.scene.layout.StackPane;
 
-import javafx.scene.paint.Color;
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+//import javafx.application.Application;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.VBox;
+//import javafx.scene.shape.Circle;
+//import javafx.scene.shape.Rectangle;
+//import javafx.stage.Stage;
+//import javafx.scene.layout.StackPane;
+//
+//import javafx.scene.paint.Color;
 
 public class Main extends Application {
 
-
-    Scene scene1, scene2;
-
     @Override
-    public void start(Stage primaryStage) {
-
-        BorderPane BPane = new BorderPane();
-        BPane.setTop(new Label("This will be at the top"));
-        BPane.setLeft(new Label("This will be at the left"));
-        BPane.setRight(new Label("This will be at the Right"));
-        BPane.setCenter(new Label("This will be at the Centre"));
-        BPane.setBottom(new Label("This will be at the bottom"));
-        Scene scene = new Scene(BPane,600,400);
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-//    public static void main (String[] args)
-//    {
-//        launch(args);
-//    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
