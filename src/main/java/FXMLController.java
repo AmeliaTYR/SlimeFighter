faf591c4cf04c153875b9e465847cb490e2c39bb
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-17 00:05:49 +0800

    Create basic user login page

--- src/main/java/FXMLController.java
+++ src/main/java/FXMLController.java
@@ -2,12 +2,19 @@ package main.java;
 
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.animation.RotateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point3D;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 import javafx.scene.shape.Arc;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 
 /**
@@ -17,16 +24,54 @@ import javafx.util.Duration;
 public class FXMLController implements Initializable {
 
     @FXML
-    private Arc arc1;
+    private TextField newUsername;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        RotateTransition transition = new RotateTransition(Duration.seconds(5), arc1);
-        transition.setAxis(new Point3D(50, 50, 0));
-        transition.setByAngle(200);
-        transition.play();
+
+    }
+
+    @FXML
+    protected void handleCreateUserButtonAction(ActionEvent event) {
+        System.out.println("Create new user:");
+        String username = newUsername.getText();
+        System.out.println(username);
+
+        // check if username already exists
+        if (username.equals("ames")) {
+            usernameExistsAlert();
+
+        }
+
+        // else procceed to create new user
+    }
+
+    @FXML
+    protected void handleUserLoginButtonAction(ActionEvent event) {
+        System.out.println("User login");
+    }
+
+    /**
+     *  Alert user if username already exists
+     */
+    public void usernameExistsAlert(){
+
+        Alert alert = new Alert(Alert.AlertType.WARNING,"", ButtonType.YES, ButtonType.NO);  //new alert object
+        alert.setTitle("Warning!");  //warning box title
+        alert.setHeaderText("Username taken!");// Header
+        alert.setContentText("Username already exists. Login instead?"); //Discription of warning
+        alert.getDialogPane().setPrefSize(300, 150); //sets size of alert box
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.YES){
+            System.out.println("user chose YES");
+        } else {
+            System.out.println("user chose NO or closed the dialog");
+
+        }
+
     }
 }
