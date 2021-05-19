4e431e6588e54b4061ce5dfbf44742da4d30f73a
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-19 22:04:07 +0800

    Remove redundant imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import main.java.characters.Player;
 
-//import java.io.IOException;
-//import java.util.Scanner;
-//
-//import javafx.application.Application;
-//import javafx.event.ActionEvent;
-//import javafx.event.EventHandler;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.scene.control.Button;
-//import javafx.scene.control.Label;
-//import javafx.scene.layout.BorderPane;
-//import javafx.scene.layout.VBox;
-//import javafx.scene.shape.Circle;
-//import javafx.scene.shape.Rectangle;
-//import javafx.stage.Stage;
-//import javafx.scene.layout.StackPane;
-//
-//import javafx.scene.paint.Color;
+import java.io.File;
 
 public class Main extends Application {
 
@@ -33,9 +16,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage loginStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("../resources/fxml/login.fxml"));
+        Image image = new Image(new File("SlimeFighter.svg").toURI().toString());
+        loginStage.getIcons().add(image);
         loginStage.setTitle("Slime Fighter");
         loginStage.setScene(new Scene(root, 600, 400));
+
         loginStage.show();
 
     }
