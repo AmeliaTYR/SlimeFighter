70ae67521a5f5fc5f2f85c1bd3b7ca310ce74e12
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-05-29 00:20:59 +0800

    Start blank Swing page to return to game

--- src/main/java/controllers/userPageController.java
+++ src/main/java/controllers/userPageController.java
@@ -2,9 +2,13 @@ package main.java.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import main.java.Main;
 
+import javax.swing.*;
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,6 +34,9 @@ public class userPageController implements Initializable {
     @FXML
     private Label defNumLabel;
 
+    @FXML
+    private Button rtnToGameBtn;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Todo: add user Profile pic code
@@ -42,6 +49,37 @@ public class userPageController implements Initializable {
         hpNumLabel.setText(Integer.toString(Main.currentUser.getHp()));
         mpNumLabel.setText(Integer.toString(Main.currentUser.getMp()));
         defNumLabel.setText(Integer.toString(Main.currentUser.getDef()));
+    }
+
+
+    @FXML
+    protected void handleResumeGameButtonAction() {
+        ImageIcon img = new ImageIcon("src/main/resources/images/SlimeFighterTransparent.png");
+        JFrame frame = new JFrame("Slime Fighters");
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        frame.setIconImage(img.getImage());
+
+        JLabel label = new JLabel("JFrame By Example");
+
+        JButton button = new JButton();
+        button.setText("Button");
+        frame.setIconImage(img.getImage());
+
+        //        panel.add(label);
+        //        panel.add(button);
+
+        frame.add(panel);
+        frame.setIconImage(img.getImage());
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        Stage stage = (Stage) rtnToGameBtn.getScene().getWindow();
+        stage.hide();
+
     }
 
 
